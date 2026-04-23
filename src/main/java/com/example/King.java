81c41229e8ff4e4d302f29163efeee661a2fa9c9
naz <nazikefyalew@gmail.com>
2026-04-23 17:53:59 +0000

    ;;;;

--- src/main/java/com/example/King.java
+++ src/main/java/com/example/King.java
@@ -7,106 +7,76 @@ package com.example;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-
 import javax.imageio.ImageIO;
-
-//you will need to implement two functions in this file.
 public class King extends Piece {
-
-    
-    public King(boolean isWhite, String img_file) {
-        super(isWhite,img_file);
-
-       // this.color = isWhite;
-         
-       // try {
-           // if (this.img == null) {
-               // this.img = ImageIO.read(new File(System.getProperty("user.dir")+img_file));
-            //}
-         // } catch (IOException e) {
-          //  System.out.println("File not found: " + e.getMessage());
-         // }
-    }
-    
-    
-
-    // Pre: b and start not null, start contains this piece, board is valid
-
-    // Post: returns list of squares this piece could capture, inside board, board unchanged
-    
-    // TO BE IMPLEMENTED!
-    //return a list of every square that is "controlled" by this piece. A square is controlled
-    //if the piece capture into it legally.
-    public ArrayList<Square> getControlledSquares(Board b, Square start) {
-        //return getLegalMoves(b, start);
-        ArrayList<Square>num= new ArrayList<Square>();
-        Square[][]board=b.getSquareArray();
-        int r=start.getRow();
-        int c=start.getCol();
-        for(int fr=-1;fr<=1;fr++){
-            for(int fc=-1;fc<=1;fc++){
-               int newR=r+fr;
-               int newC=c+fc;
-               if(fr==0&&fc==0){
-                continue;
-               }
-               if(newR>=0&&newR<8&&newC>=0&&newC<8){
-                Square tinySquare= board[newR][newC];
-                num.add(tinySquare);
-               }
-            }
-        }
-        return num;
-
-
-    }
-// Pre: b and start not null, start contains this piece, board is valid
-// Post: returns list of legal squares for this piece, inside board, no same-color pieces, board unchanged
-
-
-    //TO BE IMPLEMENTED!
-    //implement the move function here
-    //it's up to you how the piece moves, but at the very least the rules should be logical and it should never move off the board!
-    //returns an arraylist of squares which are legal to move to
-    //please note that your piece must have some sort of logic. Just being able to move to every square on the board is not
-    //going to score any points.
-    public ArrayList<Square> getLegalMoves(Board b, Square start){
-        ArrayList<Square>LegalMoves= new ArrayList<Square>();
-        Square[][]board=b.getSquareArray();
-        int r=start.getRow();
-        int c=start.getCol();
-        for(int fr=-1; fr<=1;fr++){
-            for(int fc=-1; fc<= 1;fc++){
-                if(fc==0&&fr==0){
-                    continue;
-                }
-        int newR=r+ fr;
-        int newC= c+fc;
-        if(newR>=0&&newR<8&&newC>=0&&newC<8){
-            Square tinySquare= board[newR][newC];
-            if(tinySquare.isOccupied()){
-                if(tinySquare.getOccupyingPiece().getColor()!=this.getColor()){
-                    LegalMoves.add(tinySquare);
-                }
-            }else{
-                                    LegalMoves.add(tinySquare);
-
-            }
-            }
-
-        }
-        }
-        return LegalMoves;
-    }
-    public String toString() {
-    return "King";
+// constructor
+public King(boolean isWhite, String img_file) {
+super(isWhite, img_file);
+}
+// Pre-condition: b is a non-null board that contains some squares. The
+// currentSquare is non-null and present in the board.
+// Post-condition: Returns all legally accessable squares by this piece. In
+// event that no squares are accessable returns an empty list.
+@Override
+public ArrayList<Square> getLegalMoves(Board b, Square currentSquare) {
+Square [][] board = b.getSquareArray();
+ArrayList<Square> legalMoves = new ArrayList<Square>();
+// Determine the row and column of the current square
+int row = currentSquare.getRow();
+int col = currentSquare.getCol();
+// Check each of the 8 squares surrounding the king
+for (int i = -1; i <= 1; i++) {
+for (int j = -1; j <= 1; j++) {
+// Skip the square the king is currently on
+if (i == 0 && j == 0) {
+continue;
+}
+// Check if the square is within the bounds of the board
+if (row + i >= 0 && row + i < board.length &&
+col + j >= 0 && col + j < board[row].length) {
+if(!board[row+i][col+j].isOccupied() ||
+board[row+i][col+j].getOccupyingPiece().getColor()!= getColor())
+// Add the square to the list of legal move squares
+legalMoves.add(board[row + i][col +
+j]);
 }
 }
-
-
-       
+}
+return legalMoves;
+}
+@Override
+public String toString() {
+return "A " + super.toString() + " king";
+}
+//precondition - board is a fully populated 8 by 8 2D array of Square objects(non-
+null)
+//postcondition - an array list of all squares this piece controlls is returned
+(squares it can attack on it's next turn)
+@Override
+public ArrayList<Square> getControlledSquares(Square[][] board, Square
+currentSquare) {
+ArrayList<Square> controlledSquares = new ArrayList<Square>();
+// Determine the row and column of the current square
+int row = currentSquare.getRow();
+int col = currentSquare.getCol();
+// Check each of the 8 squares surrounding the king
+for (int i = -1; i <= 1; i++) {
+for (int j = -1; j <= 1; j++) {
+// Skip the square the king is currently on
+if (i == 0 && j == 0) {
+continue;
+}
+// Check if the square is within the bounds of the board
+if (row + i >= 0 && row + i < board.length && col + j >= 0
+&& col + j < board[row].length) {
+// Add the square to the list of controlled squares
+controlledSquares.add(board[row + i][col + j]);
+}
+}
+}
+return controlledSquares;
+}
+}
